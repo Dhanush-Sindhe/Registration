e6b7246831070161d03e8a4d07c6b9f0d628a973
Dhanush-Sindhe <113535528+Dhanush-Sindhe@users.noreply.github.com>
2024-11-19 14:59:01 +0530

    Create registration.sql

---
+++ backend/registration.sql
@@ -0,0 +1,8 @@
+CREATE TABLE internassesment.registration (
+  id INT NOT NULL,
+  name VARCHAR(45) NOT NULL,
+  email VARCHAR(45) NOT NULL,
+  dob DATE NOT NULL,
+  phonenum VARCHAR(45) NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX email_UNIQUE (email ASC) VISIBLE); this is my database now give me crud operations in jdbc
